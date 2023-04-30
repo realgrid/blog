2323050f428410e880f817e8e9a77db8eef5f88c
Ryu <ryujt658@gmail.com>
2023-04-30 11:33:32 +0900

    update - lambda chat source

--- examples/aws/chat/lambda/index.js
+++ examples/aws/chat/lambda/index.js
@@ -1,10 +1,10 @@
 const AWS = require('aws-sdk');
 
-const ENDPOINT = 'llntdu8vob.execute-api.ap-northeast-2.amazonaws.com/production/';
+const ENDPOINT = '[endpoint]]';
 const client = new AWS.ApiGatewayManagementApi({ endpoint: ENDPOINT });
 const names = {};
 
-const sendToOne = async (id, body) => {
+const sendTo = async (id, body) => {
     try {
         await client.postToConnection({
             'ConnectionId': id,
@@ -16,7 +16,7 @@ const sendToOne = async (id, body) => {
 };
 
 const sendToAll = async (ids, body) => {
-    const all = ids.map(i => sendToOne(i, body));
+    const all = ids.map(i => sendTo(i, body));
     return Promise.all(all);
 };
 
@@ -33,9 +33,13 @@ exports.handler = async (event) => {
         switch (routeKey) {
             case "$connect": names[connectionId] = ""; break;
             case "$disconnect": delete names[connectionId]; break;
-            case "login": names[connectionId] = body.name; break;
-            case "sendTo": await sendToOne(connectionId, { msg: body.msg }); break;
-            case "sendToAll": await sendToAll(Object.keys(names), { msg: body.msg }); break;
+
+            default:
+                switch (body.action) {
+                    case "login": names[connectionId] = body.name; break;
+                    case "sednTo": await sendTo(connectionId, body); break;
+                    case "sendToAll": await sendToAll(Object.keys(names), body); break;
+                }
         }
     } catch (err) {
         console.error(err);
